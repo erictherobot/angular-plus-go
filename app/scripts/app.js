a4597997c52eb3d44d5c04a8deb121e14d89caab
Eric The Robot <erictherobot@gmail.com>
2015-01-23 14:55:24 -0500

    adding test route + changing app name

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('angularPlusGoApp', ['ngRoute'])
+angular.module('myApp', ['ngRoute'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/myroute', {
+        templateUrl: 'views/myroute.html',
+        controller: 'MyrouteCtrl'
       })
       .otherwise({
         redirectTo: '/'
