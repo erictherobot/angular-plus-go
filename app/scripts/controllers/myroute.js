a55aba5e7f96d87c43aee0d59b418d6dcd58e714
Eric The Robot <erictherobot@gmail.com>
2015-01-23 14:56:15 -0500

    adding test route from Yeoman scaffolding

---
+++ app/scripts/controllers/myroute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myApp')
+  .controller('MyrouteCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
